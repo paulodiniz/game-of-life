8bff85c37e6eb03d892fe94f76331590fe30820d
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-04-12 13:46:37 +0200

    basic

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,63 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (h1, text)
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Color
+import Html exposing (Html)
+import Html.Attributes exposing (style)
+import Time exposing (Posix)
 
+
+type Msg
+    = AnimationFrame Posix
+
+
+type alias Model =
+    Int
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    (0, Cmd.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        width =
+            500
+
+        height =
+            300
+    in
+    Canvas.toHtml ( width, height )
+        [ style "border" "1px solid black" ]
+        [ shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
+        , renderSquare
+        ]
+
+
+renderSquare =
+    shapes [ fill (Color.rgba 0 0 0 1) ]
+        [ rect ( 0, 0 ) 100 50 ]
+
+
+main : Program () Model Msg
 main =
-  h1 [] [ text "Hello World!" ]
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
