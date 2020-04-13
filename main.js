a0af6808b19efe7b0c74c31712a618e16c89322c
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-04-13 14:22:24 +0200

    basic initial life

--- main.js
+++ main.js
@@ -5230,12 +5230,8 @@ var $elm$browser$Browser$element = _Browser_element;
 var $author$project$Main$SeedLife = function (a) {
 	return {$: 'SeedLife', a: a};
 };
-var $elm$random$Random$Generator = function (a) {
-	return {$: 'Generator', a: a};
-};
-var $elm$core$Bitwise$and = _Bitwise_and;
-var $elm$core$Basics$negate = function (n) {
-	return -n;
+var $elm$random$Random$Generate = function (a) {
+	return {$: 'Generate', a: a};
 };
 var $elm$random$Random$Seed = F2(
 	function (a, b) {
@@ -5246,6 +5242,122 @@ var $elm$random$Random$next = function (_v0) {
 	var state0 = _v0.a;
 	var incr = _v0.b;
 	return A2($elm$random$Random$Seed, ((state0 * 1664525) + incr) >>> 0, incr);
+};
+var $elm$random$Random$initialSeed = function (x) {
+	var _v0 = $elm$random$Random$next(
+		A2($elm$random$Random$Seed, 0, 1013904223));
+	var state1 = _v0.a;
+	var incr = _v0.b;
+	var state2 = (state1 + x) >>> 0;
+	return $elm$random$Random$next(
+		A2($elm$random$Random$Seed, state2, incr));
+};
+var $elm$time$Time$Name = function (a) {
+	return {$: 'Name', a: a};
+};
+var $elm$time$Time$Offset = function (a) {
+	return {$: 'Offset', a: a};
+};
+var $elm$time$Time$Zone = F2(
+	function (a, b) {
+		return {$: 'Zone', a: a, b: b};
+	});
+var $elm$time$Time$customZone = $elm$time$Time$Zone;
+var $elm$time$Time$Posix = function (a) {
+	return {$: 'Posix', a: a};
+};
+var $elm$time$Time$millisToPosix = $elm$time$Time$Posix;
+var $elm$time$Time$now = _Time_now($elm$time$Time$millisToPosix);
+var $elm$time$Time$posixToMillis = function (_v0) {
+	var millis = _v0.a;
+	return millis;
+};
+var $elm$random$Random$init = A2(
+	$elm$core$Task$andThen,
+	function (time) {
+		return $elm$core$Task$succeed(
+			$elm$random$Random$initialSeed(
+				$elm$time$Time$posixToMillis(time)));
+	},
+	$elm$time$Time$now);
+var $elm$random$Random$step = F2(
+	function (_v0, seed) {
+		var generator = _v0.a;
+		return generator(seed);
+	});
+var $elm$random$Random$onEffects = F3(
+	function (router, commands, seed) {
+		if (!commands.b) {
+			return $elm$core$Task$succeed(seed);
+		} else {
+			var generator = commands.a.a;
+			var rest = commands.b;
+			var _v1 = A2($elm$random$Random$step, generator, seed);
+			var value = _v1.a;
+			var newSeed = _v1.b;
+			return A2(
+				$elm$core$Task$andThen,
+				function (_v2) {
+					return A3($elm$random$Random$onEffects, router, rest, newSeed);
+				},
+				A2($elm$core$Platform$sendToApp, router, value));
+		}
+	});
+var $elm$random$Random$onSelfMsg = F3(
+	function (_v0, _v1, seed) {
+		return $elm$core$Task$succeed(seed);
+	});
+var $elm$random$Random$Generator = function (a) {
+	return {$: 'Generator', a: a};
+};
+var $elm$random$Random$map = F2(
+	function (func, _v0) {
+		var genA = _v0.a;
+		return $elm$random$Random$Generator(
+			function (seed0) {
+				var _v1 = genA(seed0);
+				var a = _v1.a;
+				var seed1 = _v1.b;
+				return _Utils_Tuple2(
+					func(a),
+					seed1);
+			});
+	});
+var $elm$random$Random$cmdMap = F2(
+	function (func, _v0) {
+		var generator = _v0.a;
+		return $elm$random$Random$Generate(
+			A2($elm$random$Random$map, func, generator));
+	});
+_Platform_effectManagers['Random'] = _Platform_createManager($elm$random$Random$init, $elm$random$Random$onEffects, $elm$random$Random$onSelfMsg, $elm$random$Random$cmdMap);
+var $elm$random$Random$command = _Platform_leaf('Random');
+var $elm$random$Random$generate = F2(
+	function (tagger, generator) {
+		return $elm$random$Random$command(
+			$elm$random$Random$Generate(
+				A2($elm$random$Random$map, tagger, generator)));
+	});
+var $author$project$Main$Dead = {$: 'Dead'};
+var $author$project$Main$initCell = function (index) {
+	return $author$project$Main$Dead;
+};
+var $elm$core$Basics$round = _Basics_round;
+var $author$project$Main$squareSize = 50;
+var $author$project$Main$width = 400;
+var $author$project$Main$numCols = $elm$core$Basics$round($author$project$Main$width / $author$project$Main$squareSize);
+var $author$project$Main$height = 400;
+var $author$project$Main$numRows = $elm$core$Basics$round($author$project$Main$height / $author$project$Main$squareSize);
+var $author$project$Main$initialCells = function () {
+	var initializeCols = function (_v0) {
+		return $elm$core$Array$toList(
+			A2($elm$core$Array$initialize, $author$project$Main$numCols, $author$project$Main$initCell));
+	};
+	return $elm$core$Array$toList(
+		A2($elm$core$Array$initialize, $author$project$Main$numRows, initializeCols));
+}();
+var $elm$core$Bitwise$and = _Bitwise_and;
+var $elm$core$Basics$negate = function (n) {
+	return -n;
 };
 var $elm$core$Bitwise$xor = _Bitwise_xor;
 var $elm$random$Random$peel = function (_v0) {
@@ -5315,157 +5427,16 @@ var $elm$random$Random$list = F2(
 				return A4($elm$random$Random$listHelp, _List_Nil, n, gen, seed);
 			});
 	});
-var $elm$core$Basics$round = _Basics_round;
-var $author$project$Main$squareSize = 50;
-var $author$project$Main$width = 400;
-var $author$project$Main$numCols = $elm$core$Basics$round($author$project$Main$width / $author$project$Main$squareSize);
-var $author$project$Main$height = 400;
-var $author$project$Main$numRows = $elm$core$Basics$round($author$project$Main$height / $author$project$Main$squareSize);
-var $elm$random$Random$map2 = F3(
-	function (func, _v0, _v1) {
-		var genA = _v0.a;
-		var genB = _v1.a;
-		return $elm$random$Random$Generator(
-			function (seed0) {
-				var _v2 = genA(seed0);
-				var a = _v2.a;
-				var seed1 = _v2.b;
-				var _v3 = genB(seed1);
-				var b = _v3.a;
-				var seed2 = _v3.b;
-				return _Utils_Tuple2(
-					A2(func, a, b),
-					seed2);
-			});
-	});
-var $elm$random$Random$pair = F2(
-	function (genA, genB) {
-		return A3(
-			$elm$random$Random$map2,
-			F2(
-				function (a, b) {
-					return _Utils_Tuple2(a, b);
-				}),
-			genA,
-			genB);
-	});
-var $author$project$Main$aliveCells = A2(
-	$elm$random$Random$list,
-	10,
-	A2(
-		$elm$random$Random$pair,
-		A2($elm$random$Random$int, 0, $author$project$Main$numRows),
-		A2($elm$random$Random$int, 0, $author$project$Main$numCols)));
-var $elm$random$Random$Generate = function (a) {
-	return {$: 'Generate', a: a};
-};
-var $elm$random$Random$initialSeed = function (x) {
-	var _v0 = $elm$random$Random$next(
-		A2($elm$random$Random$Seed, 0, 1013904223));
-	var state1 = _v0.a;
-	var incr = _v0.b;
-	var state2 = (state1 + x) >>> 0;
-	return $elm$random$Random$next(
-		A2($elm$random$Random$Seed, state2, incr));
-};
-var $elm$time$Time$Name = function (a) {
-	return {$: 'Name', a: a};
-};
-var $elm$time$Time$Offset = function (a) {
-	return {$: 'Offset', a: a};
-};
-var $elm$time$Time$Zone = F2(
-	function (a, b) {
-		return {$: 'Zone', a: a, b: b};
-	});
-var $elm$time$Time$customZone = $elm$time$Time$Zone;
-var $elm$time$Time$Posix = function (a) {
-	return {$: 'Posix', a: a};
-};
-var $elm$time$Time$millisToPosix = $elm$time$Time$Posix;
-var $elm$time$Time$now = _Time_now($elm$time$Time$millisToPosix);
-var $elm$time$Time$posixToMillis = function (_v0) {
-	var millis = _v0.a;
-	return millis;
-};
-var $elm$random$Random$init = A2(
-	$elm$core$Task$andThen,
-	function (time) {
-		return $elm$core$Task$succeed(
-			$elm$random$Random$initialSeed(
-				$elm$time$Time$posixToMillis(time)));
-	},
-	$elm$time$Time$now);
-var $elm$random$Random$step = F2(
-	function (_v0, seed) {
-		var generator = _v0.a;
-		return generator(seed);
-	});
-var $elm$random$Random$onEffects = F3(
-	function (router, commands, seed) {
-		if (!commands.b) {
-			return $elm$core$Task$succeed(seed);
-		} else {
-			var generator = commands.a.a;
-			var rest = commands.b;
-			var _v1 = A2($elm$random$Random$step, generator, seed);
-			var value = _v1.a;
-			var newSeed = _v1.b;
-			return A2(
-				$elm$core$Task$andThen,
-				function (_v2) {
-					return A3($elm$random$Random$onEffects, router, rest, newSeed);
-				},
-				A2($elm$core$Platform$sendToApp, router, value));
-		}
-	});
-var $elm$random$Random$onSelfMsg = F3(
-	function (_v0, _v1, seed) {
-		return $elm$core$Task$succeed(seed);
-	});
-var $elm$random$Random$map = F2(
-	function (func, _v0) {
-		var genA = _v0.a;
-		return $elm$random$Random$Generator(
-			function (seed0) {
-				var _v1 = genA(seed0);
-				var a = _v1.a;
-				var seed1 = _v1.b;
-				return _Utils_Tuple2(
-					func(a),
-					seed1);
-			});
-	});
-var $elm$random$Random$cmdMap = F2(
-	function (func, _v0) {
-		var generator = _v0.a;
-		return $elm$random$Random$Generate(
-			A2($elm$random$Random$map, func, generator));
-	});
-_Platform_effectManagers['Random'] = _Platform_createManager($elm$random$Random$init, $elm$random$Random$onEffects, $elm$random$Random$onSelfMsg, $elm$random$Random$cmdMap);
-var $elm$random$Random$command = _Platform_leaf('Random');
-var $elm$random$Random$generate = F2(
-	function (tagger, generator) {
-		return $elm$random$Random$command(
-			$elm$random$Random$Generate(
-				A2($elm$random$Random$map, tagger, generator)));
-	});
-var $author$project$Main$Dead = {$: 'Dead'};
-var $author$project$Main$initialCells = function () {
-	var initCell = function (index) {
-		return $author$project$Main$Dead;
-	};
-	var initializeCols = function (_v0) {
-		return $elm$core$Array$toList(
-			A2($elm$core$Array$initialize, $author$project$Main$numCols, initCell));
-	};
-	return $elm$core$Array$toList(
-		A2($elm$core$Array$initialize, $author$project$Main$numRows, initializeCols));
-}();
 var $author$project$Main$init = function (_v0) {
 	return _Utils_Tuple2(
 		{cells: $author$project$Main$initialCells, cols: $author$project$Main$numCols, rows: $author$project$Main$numRows},
-		A2($elm$random$Random$generate, $author$project$Main$SeedLife, $author$project$Main$aliveCells));
+		A2(
+			$elm$random$Random$generate,
+			$author$project$Main$SeedLife,
+			A2(
+				$elm$random$Random$list,
+				10,
+				A2($elm$random$Random$int, 0, 1))));
 };
 var $author$project$Main$Frame = function (a) {
 	return {$: 'Frame', a: a};
@@ -5603,162 +5574,11 @@ var $elm$browser$Browser$Events$onAnimationFrameDelta = $elm$browser$Browser$Ani
 var $author$project$Main$subscriptions = function (model) {
 	return $elm$browser$Browser$Events$onAnimationFrameDelta($author$project$Main$Frame);
 };
-var $elm$core$Debug$log = _Debug_log;
 var $elm$core$Platform$Cmd$batch = _Platform_batch;
 var $elm$core$Platform$Cmd$none = $elm$core$Platform$Cmd$batch(_List_Nil);
-var $author$project$Main$Alive = {$: 'Alive'};
-var $elm$core$Array$fromListHelp = F3(
-	function (list, nodeList, nodeListSize) {
-		fromListHelp:
-		while (true) {
-			var _v0 = A2($elm$core$Elm$JsArray$initializeFromList, $elm$core$Array$branchFactor, list);
-			var jsArray = _v0.a;
-			var remainingItems = _v0.b;
-			if (_Utils_cmp(
-				$elm$core$Elm$JsArray$length(jsArray),
-				$elm$core$Array$branchFactor) < 0) {
-				return A2(
-					$elm$core$Array$builderToArray,
-					true,
-					{nodeList: nodeList, nodeListSize: nodeListSize, tail: jsArray});
-			} else {
-				var $temp$list = remainingItems,
-					$temp$nodeList = A2(
-					$elm$core$List$cons,
-					$elm$core$Array$Leaf(jsArray),
-					nodeList),
-					$temp$nodeListSize = nodeListSize + 1;
-				list = $temp$list;
-				nodeList = $temp$nodeList;
-				nodeListSize = $temp$nodeListSize;
-				continue fromListHelp;
-			}
-		}
-	});
-var $elm$core$Array$fromList = function (list) {
-	if (!list.b) {
-		return $elm$core$Array$empty;
-	} else {
-		return A3($elm$core$Array$fromListHelp, list, _List_Nil, 0);
-	}
-};
-var $elm$core$Array$bitMask = 4294967295 >>> (32 - $elm$core$Array$shiftStep);
-var $elm$core$Basics$ge = _Utils_ge;
-var $elm$core$Elm$JsArray$unsafeGet = _JsArray_unsafeGet;
-var $elm$core$Array$getHelp = F3(
-	function (shift, index, tree) {
-		getHelp:
-		while (true) {
-			var pos = $elm$core$Array$bitMask & (index >>> shift);
-			var _v0 = A2($elm$core$Elm$JsArray$unsafeGet, pos, tree);
-			if (_v0.$ === 'SubTree') {
-				var subTree = _v0.a;
-				var $temp$shift = shift - $elm$core$Array$shiftStep,
-					$temp$index = index,
-					$temp$tree = subTree;
-				shift = $temp$shift;
-				index = $temp$index;
-				tree = $temp$tree;
-				continue getHelp;
-			} else {
-				var values = _v0.a;
-				return A2($elm$core$Elm$JsArray$unsafeGet, $elm$core$Array$bitMask & index, values);
-			}
-		}
-	});
-var $elm$core$Bitwise$shiftLeftBy = _Bitwise_shiftLeftBy;
-var $elm$core$Array$tailIndex = function (len) {
-	return (len >>> 5) << 5;
-};
-var $elm$core$Array$get = F2(
-	function (index, _v0) {
-		var len = _v0.a;
-		var startShift = _v0.b;
-		var tree = _v0.c;
-		var tail = _v0.d;
-		return ((index < 0) || (_Utils_cmp(index, len) > -1)) ? $elm$core$Maybe$Nothing : ((_Utils_cmp(
-			index,
-			$elm$core$Array$tailIndex(len)) > -1) ? $elm$core$Maybe$Just(
-			A2($elm$core$Elm$JsArray$unsafeGet, $elm$core$Array$bitMask & index, tail)) : $elm$core$Maybe$Just(
-			A3($elm$core$Array$getHelp, startShift, index, tree)));
-	});
-var $elm$core$Elm$JsArray$unsafeSet = _JsArray_unsafeSet;
-var $elm$core$Array$setHelp = F4(
-	function (shift, index, value, tree) {
-		var pos = $elm$core$Array$bitMask & (index >>> shift);
-		var _v0 = A2($elm$core$Elm$JsArray$unsafeGet, pos, tree);
-		if (_v0.$ === 'SubTree') {
-			var subTree = _v0.a;
-			var newSub = A4($elm$core$Array$setHelp, shift - $elm$core$Array$shiftStep, index, value, subTree);
-			return A3(
-				$elm$core$Elm$JsArray$unsafeSet,
-				pos,
-				$elm$core$Array$SubTree(newSub),
-				tree);
-		} else {
-			var values = _v0.a;
-			var newLeaf = A3($elm$core$Elm$JsArray$unsafeSet, $elm$core$Array$bitMask & index, value, values);
-			return A3(
-				$elm$core$Elm$JsArray$unsafeSet,
-				pos,
-				$elm$core$Array$Leaf(newLeaf),
-				tree);
-		}
-	});
-var $elm$core$Array$set = F3(
-	function (index, value, array) {
-		var len = array.a;
-		var startShift = array.b;
-		var tree = array.c;
-		var tail = array.d;
-		return ((index < 0) || (_Utils_cmp(index, len) > -1)) ? array : ((_Utils_cmp(
-			index,
-			$elm$core$Array$tailIndex(len)) > -1) ? A4(
-			$elm$core$Array$Array_elm_builtin,
-			len,
-			startShift,
-			tree,
-			A3($elm$core$Elm$JsArray$unsafeSet, $elm$core$Array$bitMask & index, value, tail)) : A4(
-			$elm$core$Array$Array_elm_builtin,
-			len,
-			startShift,
-			A4($elm$core$Array$setHelp, startShift, index, value, tree),
-			tail));
-	});
-var $elm$core$Maybe$withDefault = F2(
-	function (_default, maybe) {
-		if (maybe.$ === 'Just') {
-			var value = maybe.a;
-			return value;
-		} else {
-			return _default;
-		}
-	});
-var $author$project$Main$seedLifeInCell = F2(
-	function (_v0, cells) {
-		var rowIndex = _v0.a;
-		var colIndex = _v0.b;
-		var column = $elm$core$Array$fromList(
-			A2(
-				$elm$core$Maybe$withDefault,
-				_List_Nil,
-				A2(
-					$elm$core$Array$get,
-					rowIndex,
-					$elm$core$Array$fromList(cells))));
-		var newColumn = $elm$core$Array$toList(
-			A3($elm$core$Array$set, colIndex, $author$project$Main$Alive, column));
-		return $elm$core$Array$toList(
-			A3(
-				$elm$core$Array$set,
-				rowIndex,
-				newColumn,
-				$elm$core$Array$fromList(cells)));
-	});
 var $author$project$Main$seedLife = F2(
-	function (_v0, indexes) {
-		var cells = _v0.cells;
-		return A3($elm$core$List$foldl, $author$project$Main$seedLifeInCell, cells, indexes);
+	function (model, indexes) {
+		return model.cells;
 	});
 var $author$project$Main$update = F2(
 	function (msg, model) {
@@ -5766,7 +5586,6 @@ var $author$project$Main$update = F2(
 			return _Utils_Tuple2(model, $elm$core$Platform$Cmd$none);
 		} else {
 			var indexes = msg.a;
-			var _v1 = A2($elm$core$Debug$log, 'Update sending', indexes);
 			return _Utils_Tuple2(
 				_Utils_update(
 					model,
--- src/Main.elm
+++ src/Main.elm
@@ -8,11 +8,13 @@ import Canvas.Settings exposing (..)
 import Canvas.Settings.Advanced exposing (..)
 import Color exposing (Color)
 import Html exposing (Html, div)
+import Random
 import Time exposing (Posix)
 
 
 type Msg
-    = Frame Float
+    = SeedLife (List ( Int, Int ))
+    | Frame Float
 
 
 type CellState
@@ -24,15 +26,23 @@ type alias Model =
     { rows : Int, cols : Int, cells : List (List CellState) }
 
 
+aliveCells : Random.Generator (List ( Int, Int ))
+aliveCells =
+    Random.list 10 (Random.pair (Random.int 0 numRows) (Random.int 0 numCols))
+
+
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { rows = numRows, cols = numCols, cells = initialCells }, Cmd.none )
+    ( { rows = numRows, cols = numCols, cells = initialCells }, Random.generate SeedLife aliveCells )
 
 
 initialCells =
     let
+        initCell index =
+            Dead
+
         initializeCols _ =
-            Array.initialize numCols (always Dead) |> Array.toList
+            Array.initialize numCols initCell |> Array.toList
     in
     Array.initialize numRows initializeCols |> Array.toList
 
@@ -47,6 +57,30 @@ update msg model =
     case msg of
         Frame _ ->
             ( model, Cmd.none )
+
+        SeedLife indexes ->
+            let
+                _ =
+                    Debug.log "Update sending" indexes
+            in
+            ( { model | cells = seedLife model indexes }, Cmd.none )
+
+
+seedLife : Model -> List ( Int, Int ) -> List (List CellState)
+seedLife { cells } indexes =
+    List.foldl seedLifeInCell cells indexes
+
+
+seedLifeInCell : ( Int, Int ) -> List (List CellState) -> List (List CellState)
+seedLifeInCell ( rowIndex, colIndex ) cells =
+    let
+        column =
+            cells |> Array.fromList |> Array.get rowIndex |> Maybe.withDefault([]) |> Array.fromList
+
+        newColumn
+            = Array.set colIndex Alive column |> Array.toList
+    in
+    Array.set rowIndex newColumn (cells |> Array.fromList) |> Array.toList
 
 
 numRows =
@@ -89,9 +123,13 @@ parseRow rowIndex rowCells =
 renderCell : Int -> Int -> CellState -> Renderable
 renderCell rowIndex columnIndex cell =
     let
-        case cell of
-            Alive -> Color.rgb 0 0 0
-            Dead  -> Color.rgb 1 1 1
+        color =
+            case cell of
+                Alive ->
+                    Color.rgb 0 0 0
+
+                Dead ->
+                    Color.rgb 1 1 1
     in
     renderSquare rowIndex columnIndex color
 
